fe67fc6a83a2b6e0b3a6bbc25b080f49237d4440
Jonas Tingeborn <tinjon@gmail.com>
2017-02-27 19:24:58 +0100

    Bug fix to revive the script
    The youtube layout and page structure had changed significantly enough
    underneath to break the script.
    
    No new features, only bug fixes.

--- youtube_comments_plus.user.js
+++ youtube_comments_plus.user.js
@@ -2,17 +2,18 @@
 // @name        YouTube Comments Plus
 // @namespace   jojje/gm
 // @description Fetches all comments and allows for inverting the sorting order
-// @require     http://code.jquery.com/jquery-1.9.1.min.js
+// @require     https://code.jquery.com/jquery-3.1.1.min.js
 // @include     https://www.youtube.com/watch?v=*
 // @downloadURL https://raw.githubusercontent.com/jojje/ytc-plus/master/youtube_comments_plus.user.js
 // @updateURL   https://raw.githubusercontent.com/jojje/ytc-plus/master/youtube_comments_plus.user.js
 // @homepageURL https://github.com/jojje/ytc-plus
 // @author      jojje
-// @version     1.0.1
+// @version     1.0.2
 // @grant       none
 // ==/UserScript==
 
-var anchorFilter = '.comments-order-menu';
+(function($){
+var anchorFilter = '.comment-section-sort-menu';
 var state = "idle";
 
 //==============================================
@@ -27,72 +28,101 @@ function updateProgress(n, total) {
   $('#f-bar').css('width',spct);
   $('#f-progress').show();
 
-  log("updating stats: "+ n + "/"+ total +": " + spct);  
+  log("updating stats: "+ n + "/"+ total +": " + spct);
 }
 function hideProgress() {
   $('#f-progress').fadeOut('slow');
 }
 
+function sendClickEvent(el){
+  var evt = document.createEvent('Events');
+  evt.initEvent('click', true, false);
+  el.dispatchEvent(evt);
+}
+
+function getAllLoadMoreButtons() {
+  return $('.yt-uix-expander-collapsed > .yt-uix-expander-collapsed-body > button.yt-uix-load-more:visible');
+}
+
 function fetchReplies() {
-  $('.load-comments:visible').each(function() {
-    var self = $(this), observer;
-    
-    observer = onAttributeChange(self[0], function() {  // Element changes class attribute when replies are fetched
-      if(self.hasClass('hid')) {                        //  by adding hid class.
+  getAllLoadMoreButtons().each(function() {
+    var self = $(this).closest('.yt-uix-expander-collapsed'), observer;
+
+    observer = onAttributeChange(self[0], function() {  // Element changes class attribute when replies have been fetched
+      if(!self.hasClass('.yt-uix-expander-collapsed')){ //  by removing the class.
         observer.disconnect();                          // Stop listening to this reply-set since it's now done.
         log('loaded comments for ', self[0]);
         notify('stats.stale');
-        if ( $('.load-comments:visible').length == 0) {  // last step is fetching the replies, so if there are no more to get, we're done with all the XHR fetching
+        if ( getAllLoadMoreButtons().length === 0) {    // last step is fetching the replies, so if there are no more to get, we're done with all the XHR fetching
+          observer.disconnect();
           notify('replies.fetched');
         }
       }
     });
-  }).click();                                           // Trigger fetching of all unfetched (hidden/collapsed) replies.
+  }).each(function() {
+    sendClickEvent(this);                               // Trigger fetching of all unfetched (hidden/collapsed) replies.
+  });
 }
 
 function getPager() {
-  return $('#yt-comments-paginator');
+  return $('.comment-section-renderer-paginator.load-more-button');
+}
+
+function isEnabled(el) {
+  return el[0] && !el.prop('disabled');
 }
 
 function fetchMore() {
   state = 'fetching';
-  getPager().click();
+  sendClickEvent(getPager()[0]);
+}
+
+function isTopCommentsView() {
+  return $('#comment-section-renderer-items > section').length > 0;
 }
 
 function invertOrder() {                               // Invert the order of the comments. Replies are already in a sane order (chronoligical)
-  var comments = $('.comment-entry');
+  var filter = isTopCommentsView() ? 'section.comment-thread-renderer' : '.comment-renderer';
+  var comments = $(filter);
   comments.parent().first().append( comments.get().reverse() );
 }
 
 function getTotalCommentCount() {
-  var m = $('.all-comments > a').text().trim().match(/\(([,.\d]+)\)/);
+  var m = $('.comment-section-header-renderer').text().trim().match(/([,.\d]+)/);
   if(m) return parseInt(m[1].replace(/[.,]/g,''),10);
-  log('Error: Failed to find total number of comments');  
+  log('Error: Failed to find total number of comments');
 }
 
 function getFetchedCommentCount() {
-  return $('.comment-item').length;
+  return $('.comment-renderer').length;
 }
 
 function addListeners() {
-  var pager = getPager();
-  
+  var discussion = $('#watch-discussion'), observer;
+
   $('#b-fetchall').click(function() {
-    $(this).hide();
+    $(this).hide(function(){ $(this).remove(); });
     notify('stats.stale');
     fetchMore();
   });
-  
+
   $('#b-sort').click(invertOrder);
-  
-  if(!pager.is(':visible')) notify('all.fetched');                // If there are to few comments to warrant a pager, there's nothing to fetch, so we're done.
-   
-  onAttributeChange(pager[0], function() {                        // else, there are more comments to fetch.
+
+  if(getPager()[0]) notify('all.fetched');                        // If there are to few comments to warrant a pager, there's nothing to fetch, so we're done.
+
+  observer = onAttributeChange(discussion[0], function() {        // else, there are more comments to fetch.
     if(state != 'fetching') return;                               // Only act if the paginated fetching was started using the script's button, not by user clicking "Show more".
-    if(!pager.is(':visible')) notify('all.fetched');              // We've fetched all comments.
-    else if(!pager.hasClass('activated')) notify('page.fetched'); // Google removes this class when it has injected additional comments.
-    else notify('fetching.page');                                 // Pager is visible and has the activated class, so it's currently fetching a comment page.
-  });
+    var pager = getPager();
+
+    if(!pager[0]) {                                               // We've fetched all comments when the pager is gone.
+      observer.disconnect();
+      notify('all.fetched');
+    } else if(!isEnabled(pager)) {                                // Google disables the pager during fetching of next page.
+      notify('fetching.page');
+    } else {                                                      // Pager is visible and has the activated class, so it's currently fetching a comment page.
+      notify('page.fetched');
+    }
+  }, true);
 
   on('page.fetched', function(){
     notify('stats.stale');
@@ -117,7 +147,7 @@ function addListeners() {
 //==============================================
 
 function log() {
-  return;
+  // return;
   if(typeof(console) != 'undefined' && console.debug) {
     var stamp = (new Date()).toLocaleString().split(' ').pop(),
         args  = Array.prototype.slice.call(arguments);
@@ -135,22 +165,25 @@ function addUI() {
     '</style>').appendTo('body');
 
   $('<div id="c-fetchall">').insertAfter(anchorFilter);
-  
+
   $('<button id="b-sort" class="yt-uix-button yt-uix-button-default" title="Reverse the comment sorting order"><span>Reverse</span></button>')
   .appendTo('#c-fetchall');
-  
+
   $('<button id="b-fetchall" class="yt-uix-button yt-uix-button-default" title="Fetch all comments and replies"><span>Fetch all</span></button>')
   .appendTo('#c-fetchall');
-  
+
   $('<div id="f-progress" style="display:none"><span id="f-percent">50%</span><div id="f-bar"></div></div>')
   .appendTo('#c-fetchall');
 }
 
-function onAttributeChange(el,cb) {
+// If chidren is truthy, monitors subtree child node additions and removal, else attribute modification directly on the provided 'el'ement.
+function onAttributeChange(el, cb, children) {
   var MutationObserver = window.MutationObserver || window.WebKitMutationObserver,
       observer = new MutationObserver(cb);
   observer.observe(el, {
-    attributes: true
+    attributes: !children,
+    childList: !!children,
+    subtree: !!children,
   });
   return observer;
 }
@@ -177,3 +210,6 @@ onReady(function() {
   addListeners();
   log('yt enhancement initialized');
 });
+
+})($);
+$.noConflict();
